871faa9ff4495171bc4f237d3366f7105b26750e
Nils Christian Ehmke <nie@informatik.uni-kiel.de>
2015-02-21 12:10:26 +0100

    Refactoring

--- src/test/java/kieker/diagnosis/common/model/importer/stages/LegacyTraceReconstructorTest.java
+++ src/test/java/kieker/diagnosis/common/model/importer/stages/LegacyTraceReconstructorTest.java
@@ -1,5 +1,5 @@
 /***************************************************************************
- * Copyright 2014 Kieker Project (http://kieker-monitoring.net)
+ * Copyright 2014 Kieker Project (http://kiekercoir-monitoring.net)
  *
  * Licensed under the Apache License, Version 2.0 (the "License");
  * you may not use this file except in compliance with the License.
@@ -16,6 +16,7 @@
 
 package kieker.diagnosis.common.model.importer.stages;
 
+import static kieker.diagnosis.common.model.importer.stages.StageTester.testStageBySending;
 import static org.hamcrest.Matchers.is;
 import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
 import static org.junit.Assert.assertThat;
@@ -32,13 +33,10 @@ import org.junit.Test;
 import teetime.framework.Analysis;
 import teetime.framework.AnalysisConfiguration;
 import teetime.framework.pipe.IPipeFactory;
-import teetime.framework.pipe.PipeFactoryRegistry.PipeOrdering;
-import teetime.framework.pipe.PipeFactoryRegistry.ThreadCommunication;
 import teetime.framework.pipe.SingleElementPipeFactory;
 import teetime.stage.CollectorSink;
 import teetime.stage.InitialElementProducer;
 import teetime.stage.InstanceOfFilter;
-import teetime.stage.IterableProducer;
 import teetime.stage.className.ClassNameRegistryRepository;
 import teetime.stage.io.filesystem.Dir2RecordsFilter;
 
@@ -46,52 +44,49 @@ public class LegacyTraceReconstructorTest {
 
 	@Test
 	public void singleCallReconstructionShouldWork() throws Exception {
-		final List<OperationExecutionRecord> input = new ArrayList<>();
-		input.add(new OperationExecutionRecord("operation", OperationExecutionRecord.NO_SESSION_ID, 42, 15L, 20L, "localhost", 0, 0));
+		final List<OperationExecutionRecord> records = new ArrayList<>();
+		records.add(new OperationExecutionRecord("operation", OperationExecutionRecord.NO_SESSION_ID, 42, 15L, 20L, "localhost", 0, 0));
 
-		final ReconstructionConfiguration configuration = new ReconstructionConfiguration(input);
-		final Analysis analysis = new Analysis(configuration);
-		analysis.start();
+		final LegacyTraceReconstructor reconstructor = new LegacyTraceReconstructor();
+		List<Trace> result = testStageBySending(records).to(reconstructor.getInputPort()).andReceivingFrom(reconstructor.getOutputPort());
 
-		assertThat(configuration.getOutput(), hasSize(1));
-		assertThat(configuration.getOutput().get(0).getRootOperationCall().getOperation(), is("operation"));
-		assertThat(configuration.getOutput().get(0).getRootOperationCall().getDuration(), is(5L));
-		assertThat(configuration.getOutput().get(0).getRootOperationCall().getTraceID(), is(42L));
+		assertThat(result, hasSize(1));
+		assertThat(result.get(0).getRootOperationCall().getOperation(), is("operation"));
+		assertThat(result.get(0).getRootOperationCall().getDuration(), is(5L));
+		assertThat(result.get(0).getRootOperationCall().getTraceID(), is(42L));
 	}
 
 	@Test
 	public void separationOfComponentShouldWork() throws Exception {
-		final List<OperationExecutionRecord> input = new ArrayList<>();
-		input.add(new OperationExecutionRecord("bookstoreTracing.Catalog.getBook(boolean)", "1", 42, 15L, 20L, "SRV1", 0, 0));
+		final List<OperationExecutionRecord> records = new ArrayList<>();
+		records.add(new OperationExecutionRecord("bookstoreTracing.Catalog.getBook(boolean)", "1", 42, 15L, 20L, "SRV1", 0, 0));
 
-		final ReconstructionConfiguration configuration = new ReconstructionConfiguration(input);
-		final Analysis analysis = new Analysis(configuration);
-		analysis.start();
+		final LegacyTraceReconstructor reconstructor = new LegacyTraceReconstructor();
+		List<Trace> result = testStageBySending(records).to(reconstructor.getInputPort()).andReceivingFrom(reconstructor.getOutputPort());
 
-		assertThat(configuration.getOutput(), hasSize(1));
-		assertThat(configuration.getOutput().get(0).getRootOperationCall().getContainer(), is("SRV1"));
-		assertThat(configuration.getOutput().get(0).getRootOperationCall().getComponent(), is("bookstoreTracing.Catalog"));
-		assertThat(configuration.getOutput().get(0).getRootOperationCall().getOperation(), is("bookstoreTracing.Catalog.getBook(boolean)"));
+		assertThat(result, hasSize(1));
+		assertThat(result.get(0).getRootOperationCall().getContainer(), is("SRV1"));
+		assertThat(result.get(0).getRootOperationCall().getComponent(), is("bookstoreTracing.Catalog"));
+		assertThat(result.get(0).getRootOperationCall().getOperation(), is("bookstoreTracing.Catalog.getBook(boolean)"));
 	}
 
 	@Test
 	public void nestedCallReconstructionShouldWork() throws Exception {
-		final List<OperationExecutionRecord> input = new ArrayList<>();
-		input.add(new OperationExecutionRecord("B", OperationExecutionRecord.NO_SESSION_ID, 42, 10L, 20L, "localhost", 1, 1));
-		input.add(new OperationExecutionRecord("C", OperationExecutionRecord.NO_SESSION_ID, 42, 10L, 20L, "localhost", 2, 1));
-		input.add(new OperationExecutionRecord("D", OperationExecutionRecord.NO_SESSION_ID, 42, 10L, 20L, "localhost", 3, 2));
-		input.add(new OperationExecutionRecord("A", OperationExecutionRecord.NO_SESSION_ID, 42, 10L, 20L, "localhost", 0, 0));
+		final List<OperationExecutionRecord> records = new ArrayList<>();
+		records.add(new OperationExecutionRecord("B", OperationExecutionRecord.NO_SESSION_ID, 42, 10L, 20L, "localhost", 1, 1));
+		records.add(new OperationExecutionRecord("C", OperationExecutionRecord.NO_SESSION_ID, 42, 10L, 20L, "localhost", 2, 1));
+		records.add(new OperationExecutionRecord("D", OperationExecutionRecord.NO_SESSION_ID, 42, 10L, 20L, "localhost", 3, 2));
+		records.add(new OperationExecutionRecord("A", OperationExecutionRecord.NO_SESSION_ID, 42, 10L, 20L, "localhost", 0, 0));
 
-		final ReconstructionConfiguration configuration = new ReconstructionConfiguration(input);
-		final Analysis analysis = new Analysis(configuration);
-		analysis.start();
+		final LegacyTraceReconstructor reconstructor = new LegacyTraceReconstructor();
+		List<Trace> result = testStageBySending(records).to(reconstructor.getInputPort()).andReceivingFrom(reconstructor.getOutputPort());
 
-		assertThat(configuration.getOutput(), hasSize(1));
-		assertThat(configuration.getOutput().get(0).getRootOperationCall().getOperation(), is("A"));
-		assertThat(configuration.getOutput().get(0).getRootOperationCall().getDuration(), is(10L));
-		assertThat(configuration.getOutput().get(0).getRootOperationCall().getChildren().get(0).getOperation(), is("B"));
-		assertThat(configuration.getOutput().get(0).getRootOperationCall().getChildren().get(1).getOperation(), is("C"));
-		assertThat(configuration.getOutput().get(0).getRootOperationCall().getChildren().get(1).getChildren().get(0).getOperation(), is("D"));
+		assertThat(result, hasSize(1));
+		assertThat(result.get(0).getRootOperationCall().getOperation(), is("A"));
+		assertThat(result.get(0).getRootOperationCall().getDuration(), is(10L));
+		assertThat(result.get(0).getRootOperationCall().getChildren().get(0).getOperation(), is("B"));
+		assertThat(result.get(0).getRootOperationCall().getChildren().get(1).getOperation(), is("C"));
+		assertThat(result.get(0).getRootOperationCall().getChildren().get(1).getChildren().get(0).getOperation(), is("D"));
 	}
 
 	@Test
@@ -101,28 +96,6 @@ public class LegacyTraceReconstructorTest {
 		analysis.start();
 
 		assertThat(configuration.getOutput(), hasSize(1635));
-	}
-
-	private static class ReconstructionConfiguration extends AnalysisConfiguration {
-
-		private final List<Trace> traceCollectorList = new ArrayList<>();
-
-		public ReconstructionConfiguration(final List<OperationExecutionRecord> input) {
-			final IterableProducer<OperationExecutionRecord> producer = new IterableProducer<>(input);
-			final LegacyTraceReconstructor reconstructor = new LegacyTraceReconstructor();
-			final CollectorSink<Trace> collector = new CollectorSink<>(this.traceCollectorList);
-
-			final IPipeFactory pipeFactory = AnalysisConfiguration.PIPE_FACTORY_REGISTRY.getPipeFactory(ThreadCommunication.INTRA, PipeOrdering.ARBITRARY, false);
-			pipeFactory.create(producer.getOutputPort(), reconstructor.getInputPort());
-			pipeFactory.create(reconstructor.getOutputPort(), collector.getInputPort());
-
-			this.addThreadableStage(producer);
-		}
-
-		public List<Trace> getOutput() {
-			return this.traceCollectorList;
-		}
-
 	}
 
 	private static class ExampleLogReconstructionConfiguration extends AnalysisConfiguration {
