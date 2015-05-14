251958f0de2e7ec3b3938543f4065116902856ac
Nils Christian Ehmke <nils@rhocas.de>
2015-05-14 11:55:56 +0200

    Added missing JavaDoc and license texts

--- src/main/java/kieker/diagnosis/mainview/subview/util/LazyOperationCallTreeItem.java
+++ src/main/java/kieker/diagnosis/mainview/subview/util/LazyOperationCallTreeItem.java
@@ -1,3 +1,19 @@
+/***************************************************************************
+ * Copyright 2015 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
 package kieker.diagnosis.mainview.subview.util;
 
 import java.util.ArrayList;
@@ -7,6 +23,9 @@ import javafx.collections.ObservableList;
 import javafx.scene.control.TreeItem;
 import kieker.diagnosis.domain.AbstractOperationCall;
 
+/**
+ * @author Nils Christian Ehmke
+ */
 public final class LazyOperationCallTreeItem<T extends AbstractOperationCall<T>> extends TreeItem<T> {
 
 	private boolean childrenInitialized = false;
