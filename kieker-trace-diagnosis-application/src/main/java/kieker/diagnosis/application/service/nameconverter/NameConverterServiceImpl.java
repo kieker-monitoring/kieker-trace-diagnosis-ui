8fb95c1cd09b8c259c23e50326c539bb64b11f28
Nils Christian Ehmke <nils@rhocas.de>
2017-03-13 18:53:27 +0100

    Fixed some Checkstyle issues

--- kieker-trace-diagnosis-application/src/main/java/kieker/diagnosis/application/service/nameconverter/NameConverterServiceImpl.java
+++ kieker-trace-diagnosis-application/src/main/java/kieker/diagnosis/application/service/nameconverter/NameConverterServiceImpl.java
@@ -1,4 +1,25 @@
+/***************************************************************************
+ * Copyright 2015-2016 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
 package kieker.diagnosis.application.service.nameconverter;
+
+import kieker.diagnosis.application.service.properties.TimestampProperty;
+import kieker.diagnosis.application.service.properties.TimestampTypes;
+import kieker.diagnosis.architecture.service.properties.PropertiesService;
+import kieker.diagnosis.architecture.util.Mapper;
 
 import java.time.Instant;
 import java.time.ZoneId;
@@ -9,11 +30,6 @@ import java.util.concurrent.TimeUnit;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Component;
-
-import kieker.diagnosis.application.service.properties.TimestampProperty;
-import kieker.diagnosis.application.service.properties.TimestampTypes;
-import kieker.diagnosis.architecture.service.properties.PropertiesService;
-import kieker.diagnosis.architecture.util.Mapper;
 
 @Component
 class NameConverterServiceImpl implements NameConverterService {
@@ -34,21 +50,25 @@ class NameConverterServiceImpl implements NameConverterService {
 		ivShortTimeUnitMapper.mapPerDefault( ).to( "" );
 	}
 
+	@Override
 	public String toShortTimeUnit( final TimeUnit aTimeUnit ) {
 		return ivShortTimeUnitMapper.resolve( aTimeUnit );
 	}
 
+	@Override
 	public String toShortComponentName( final String aComponentName ) {
 		final int lastPointPos = aComponentName.lastIndexOf( '.' );
 		return aComponentName.substring( lastPointPos + 1 );
 	}
 
+	@Override
 	public String toShortOperationName( final String aOperationName ) {
 		final String result = aOperationName.replaceAll( "\\(.*\\)", "(...)" );
 		final int lastPointPos = result.lastIndexOf( '.', result.length( ) - 5 );
 		return result.substring( lastPointPos + 1 );
 	}
 
+	@Override
 	public String toDurationString( final long aDuration, final TimeUnit aSourceUnit, final TimeUnit aTargetUnit ) {
 		final String shortSourceUnit = toShortTimeUnit( aSourceUnit );
 		final String shortTargetUnit = toShortTimeUnit( aTargetUnit );
@@ -58,6 +78,7 @@ class NameConverterServiceImpl implements NameConverterService {
 		return String.format( "%d %s (%d %s)", targetDuration, shortTargetUnit, aDuration, shortSourceUnit );
 	}
 
+	@Override
 	public String toTimestampString( final long aTimestamp, final TimeUnit aSourceUnit ) {
 		final TimestampTypes timestampType = ivPropertiesService.loadApplicationProperty( TimestampProperty.class );
 		if ( timestampType == TimestampTypes.TIMESTAMP ) {
