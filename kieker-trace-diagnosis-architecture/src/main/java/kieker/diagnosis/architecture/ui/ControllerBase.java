7d5c9c0a96b752e394fe65bb999fb78e5855ef91
Nils Christian Ehmke <nils@rhocas.de>
2018-03-30 14:17:38 +0200

    [TRACEUI-18] Controllers are using the wrong logger
    Signed-off-by: Nils Christian Ehmke <nils@rhocas.de>

--- kieker-trace-diagnosis-architecture/src/main/java/kieker/diagnosis/architecture/ui/ControllerBase.java
+++ kieker-trace-diagnosis-architecture/src/main/java/kieker/diagnosis/architecture/ui/ControllerBase.java
@@ -1,17 +1,17 @@
-/*************************************************************************** 
- * Copyright 2015-2018 Kieker Project (http://kieker-monitoring.net)         
- *                                                                           
- * Licensed under the Apache License, Version 2.0 (the "License");           
- * you may not use this file except in compliance with the License.          
- * You may obtain a copy of the License at                                   
- *                                                                           
- *     http://www.apache.org/licenses/LICENSE-2.0                            
- *                                                                           
- * Unless required by applicable law or agreed to in writing, software       
- * distributed under the License is distributed on an "AS IS" BASIS,         
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
- * See the License for the specific language governing permissions and       
- * limitations under the License.                                            
+/***************************************************************************
+ * Copyright 2015-2018 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  ***************************************************************************/
 
 package kieker.diagnosis.architecture.ui;
@@ -40,7 +40,7 @@ public abstract class ControllerBase<VM extends ViewModelBase<?>> {
 
 	private final ResourceBundle ivResourceBundle = ResourceBundle.getBundle( ClassUtil.getRealName( getClass( ) ) );
 
-	private static Logger cvLogger;
+	private Logger ivLogger;
 
 	@Inject
 	private VM ivViewModel;
@@ -80,11 +80,11 @@ public abstract class ControllerBase<VM extends ViewModelBase<?>> {
 	 * @return The logger.
 	 */
 	protected final Logger getLogger( ) {
-		if ( cvLogger == null ) {
-			cvLogger = LogManager.getLogger( ClassUtil.getRealClass( getClass( ) ) );
+		if ( ivLogger == null ) {
+			ivLogger = LogManager.getLogger( ClassUtil.getRealClass( getClass( ) ) );
 		}
 
-		return cvLogger;
+		return ivLogger;
 	}
 
 	/**
