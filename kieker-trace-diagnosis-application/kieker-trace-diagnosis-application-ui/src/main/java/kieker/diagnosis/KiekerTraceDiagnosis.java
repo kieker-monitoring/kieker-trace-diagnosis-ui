a3faf11590d6cc48d1092cd1ad0cd964d572146c
Nils Christian Ehmke <nils@rhocas.de>
2018-04-02 09:03:49 +0200

    Added dependencies and license files for mvvmFX
    Signed-off-by: Nils Christian Ehmke <nils@rhocas.de>

--- kieker-trace-diagnosis-application/kieker-trace-diagnosis-application-ui/src/main/java/kieker/diagnosis/KiekerTraceDiagnosis.java
+++ kieker-trace-diagnosis-application/kieker-trace-diagnosis-application-ui/src/main/java/kieker/diagnosis/KiekerTraceDiagnosis.java
@@ -1,17 +1,17 @@
-/*************************************************************************** 
- * Copyright 2015-2018 Kieker Project (http://kieker-monitoring.net)         
- *                                                                           
- * Licensed under the Apache License, Version 2.0 (the "License");           
- * you may not use this file except in compliance with the License.          
- * You may obtain a copy of the License at                                   
- *                                                                           
- *     http://www.apache.org/licenses/LICENSE-2.0                            
- *                                                                           
- * Unless required by applicable law or agreed to in writing, software       
- * distributed under the License is distributed on an "AS IS" BASIS,         
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.  
- * See the License for the specific language governing permissions and       
- * limitations under the License.                                            
+/***************************************************************************
+ * Copyright 2015-2018 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
  ***************************************************************************/
 
 package kieker.diagnosis;
@@ -22,7 +22,7 @@ import java.util.ResourceBundle;
 import com.google.inject.Guice;
 import com.google.inject.Injector;
 
-import javafx.application.Application;
+import de.saxsys.mvvmfx.guice.MvvmfxGuiceApplication;
 import javafx.scene.Scene;
 import javafx.scene.image.Image;
 import javafx.stage.Stage;
@@ -34,14 +34,14 @@ import kieker.diagnosis.ui.main.MainView;
  *
  * @author Nils Christian Ehmke
  */
-public class KiekerTraceDiagnosis extends Application {
+public class KiekerTraceDiagnosis extends MvvmfxGuiceApplication {
 
 	public static void main( final String[] aArgs ) {
-		Application.launch( aArgs );
+		launch( aArgs );
 	}
 
 	@Override
-	public void start( final Stage aPrimaryStage ) throws Exception {
+	public void startMvvmfx( final Stage aPrimaryStage ) throws Exception {
 		// Load the CDI container
 		final KiekerTraceDiagnosisUIModule module = new KiekerTraceDiagnosisUIModule( );
 		final Injector injector = Guice.createInjector( com.google.inject.Stage.PRODUCTION, module );
