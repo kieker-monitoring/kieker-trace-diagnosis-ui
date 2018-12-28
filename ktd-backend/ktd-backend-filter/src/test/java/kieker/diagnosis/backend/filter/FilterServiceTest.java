f48183ec908392d5a5accc0d76e9dfadab4b8624
Nils Christian Ehmke <nils@rhocas.de>
2018-12-28 17:50:36 +0100

    Refactoring
    Signed-off-by: Nils Christian Ehmke <nils@rhocas.de>

--- ktd-backend/ktd-backend-filter/src/test/java/kieker/diagnosis/backend/filter/FilterServiceTest.java
+++ ktd-backend/ktd-backend-filter/src/test/java/kieker/diagnosis/backend/filter/FilterServiceTest.java
@@ -19,7 +19,10 @@ package kieker.diagnosis.backend.filter;
 import static org.hamcrest.core.Is.is;
 import static org.junit.Assert.assertThat;
 
+import java.time.LocalDate;
+import java.time.LocalTime;
 import java.util.Arrays;
+import java.util.Calendar;
 import java.util.function.Predicate;
 
 import org.junit.Before;
@@ -98,6 +101,100 @@ public final class FilterServiceTest {
 		assertThat( conjunction.test( 30 ), is( true ) );
 		assertThat( conjunction.test( 9 ), is( false ) );
 		assertThat( conjunction.test( 4 ), is( false ) );
+	}
+
+	@Test
+	public void testCreateAfterTimePredicate( ) {
+		final Predicate<Long> predicate = filterService.createAfterTimePredicate( e -> e, LocalDate.of( 2000, 05, 01 ), LocalTime.of( 15, 20 ) );
+
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 25 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2001, 05, 01, 10, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 1999, 05, 01, 15, 20 ) ), is( false ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 14, 20 ) ), is( false ) );
+	}
+
+	@Test
+	public void testCreateAfterTimePredicateDateNull( ) {
+		final Predicate<Long> predicate = filterService.createAfterTimePredicate( e -> e, null, LocalTime.of( 15, 20 ) );
+
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 25 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2001, 05, 01, 10, 20 ) ), is( false ) );
+		assertThat( predicate.test( createTimestamp( 1999, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 14, 20 ) ), is( false ) );
+	}
+
+	@Test
+	public void testCreateAfterTimePredicateTimeNull( ) {
+		final Predicate<Long> predicate = filterService.createAfterTimePredicate( e -> e, LocalDate.of( 2000, 05, 01 ), null );
+
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 25 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2001, 05, 01, 10, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 1999, 05, 01, 15, 20 ) ), is( false ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 14, 20 ) ), is( true ) );
+	}
+
+	@Test
+	public void testCreateAfterTimePredicateDateAndTimeNull( ) {
+		final Predicate<Long> predicate = filterService.createAfterTimePredicate( e -> e, null, null );
+
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 25 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2001, 05, 01, 10, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 1999, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 14, 20 ) ), is( true ) );
+	}
+
+	@Test
+	public void testCreateBeforeTimePredicate( ) {
+		final Predicate<Long> predicate = filterService.createBeforeTimePredicate( e -> e, LocalDate.of( 2000, 05, 01 ), LocalTime.of( 15, 20 ) );
+
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 25 ) ), is( false ) );
+		assertThat( predicate.test( createTimestamp( 2001, 05, 01, 10, 20 ) ), is( false ) );
+		assertThat( predicate.test( createTimestamp( 1999, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 14, 20 ) ), is( true ) );
+	}
+
+	@Test
+	public void testCreateBeforeTimePredicateDateNull( ) {
+		final Predicate<Long> predicate = filterService.createBeforeTimePredicate( e -> e, null, LocalTime.of( 15, 20 ) );
+
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 25 ) ), is( false ) );
+		assertThat( predicate.test( createTimestamp( 2001, 05, 01, 10, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 1999, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 14, 20 ) ), is( true ) );
+	}
+
+	@Test
+	public void testCreateBeforeTimePredicateTimeNull( ) {
+		final Predicate<Long> predicate = filterService.createBeforeTimePredicate( e -> e, LocalDate.of( 2000, 05, 01 ), null );
+
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 25 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2001, 05, 01, 10, 20 ) ), is( false ) );
+		assertThat( predicate.test( createTimestamp( 1999, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 14, 20 ) ), is( true ) );
+	}
+
+	@Test
+	public void testCreateBeforeTimePredicateDateAndTimeNull( ) {
+		final Predicate<Long> predicate = filterService.createBeforeTimePredicate( e -> e, null, null );
+
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 15, 25 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2001, 05, 01, 10, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 1999, 05, 01, 15, 20 ) ), is( true ) );
+		assertThat( predicate.test( createTimestamp( 2000, 05, 01, 14, 20 ) ), is( true ) );
+	}
+
+	private long createTimestamp( final int aYear, final int aMonth, final int aDay, final int aHour, final int aMinute ) {
+		final Calendar calendar = Calendar.getInstance( );
+		calendar.set( aYear, aMonth - 1, aDay, aHour, aMinute, 0 );
+		return calendar.getTimeInMillis( );
 	}
 
 }
