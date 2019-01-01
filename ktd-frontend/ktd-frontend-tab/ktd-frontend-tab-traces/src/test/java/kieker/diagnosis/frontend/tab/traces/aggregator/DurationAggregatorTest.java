3032248c35da7a9b368ef05697339306d9132a16
Nils Christian Ehmke <nils@rhocas.de>
2019-01-01 15:39:53 +0100

    Refactoring
    Signed-off-by: Nils Christian Ehmke <nils@rhocas.de>

---
+++ ktd-frontend/ktd-frontend-tab/ktd-frontend-tab-traces/src/test/java/kieker/diagnosis/frontend/tab/traces/aggregator/DurationAggregatorTest.java
@@ -0,0 +1,78 @@
+/***************************************************************************
+ * Copyright 2015-2018 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.diagnosis.frontend.tab.traces.aggregator;
+
+import static org.hamcrest.collection.IsCollectionWithSize.hasSize;
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+import static org.junit.Assert.assertTrue;
+
+import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
+
+import org.junit.Test;
+
+import kieker.diagnosis.backend.data.MethodCall;
+
+/**
+ * This is a unit test for {@link DurationAggregator}.
+ *
+ * @author Nils Christian Ehmke
+ */
+public final class DurationAggregatorTest {
+
+	@Test
+	public void testAggregationOnEmptyList( ) {
+		final Aggregator aggregator = new DurationAggregator( 1 );
+
+		assertThat( aggregator.aggregate( Collections.emptyList( ) ), is( empty( ) ) );
+	}
+
+	@Test
+	public void testAggregationOnSingletonList( ) {
+		final Aggregator aggregator = new DurationAggregator( 1 );
+
+		final MethodCall call = new MethodCall( );
+		assertThat( aggregator.aggregate( Collections.singletonList( call ) ), contains( call ) );
+	}
+
+	@Test
+	public void testRealAggregation( ) {
+		final Aggregator aggregator = new DurationAggregator( 1 );
+
+		final MethodCall call1 = new MethodCall( );
+		call1.setDuration( 1 );
+
+		final MethodCall call2 = new MethodCall( );
+		call2.setDuration( 2 );
+
+		final MethodCall call3 = new MethodCall( );
+		call3.setDuration( 42 );
+
+		final List<MethodCall> aggregatedList = aggregator.aggregate( Arrays.asList( call1, call2, call3 ) );
+		assertThat( aggregatedList, hasSize( 2 ) );
+		assertTrue( aggregatedList.contains( call3 ) );
+
+		final MethodCall aggregationMethodCall = aggregatedList.get( 1 );
+		assertThat( aggregationMethodCall.getMethod( ), is( "2 Methodenaufrufe zusammengefasst " ) );
+		assertThat( aggregationMethodCall.getDuration( ), is( 3L ) );
+	}
+
+}
---
+++ ktd-frontend/ktd-frontend-tab/ktd-frontend-tab-traces/src/test/java/kieker/diagnosis/frontend/tab/traces/aggregator/IdentityAggregatorTest.java
@@ -0,0 +1,52 @@
+/***************************************************************************
+ * Copyright 2015-2018 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
+package kieker.diagnosis.frontend.tab.traces.aggregator;
+
+import static org.hamcrest.collection.IsEmptyCollection.empty;
+import static org.hamcrest.collection.IsIterableContainingInOrder.contains;
+import static org.hamcrest.core.Is.is;
+import static org.junit.Assert.assertThat;
+
+import java.util.Collections;
+
+import org.junit.Test;
+
+import kieker.diagnosis.backend.data.MethodCall;
+
+/**
+ * This is a unit test for {@link IdentityAggregator}.
+ *
+ * @author Nils Christian Ehmke
+ */
+public final class IdentityAggregatorTest {
+
+	@Test
+	public void testAggregationOnEmptyList( ) {
+		final Aggregator aggregator = new IdentityAggregator( );
+
+		assertThat( aggregator.aggregate( Collections.emptyList( ) ), is( empty( ) ) );
+	}
+
+	@Test
+	public void testAggregationOnSingletonList( ) {
+		final Aggregator aggregator = new IdentityAggregator( );
+
+		final MethodCall call = new MethodCall( );
+		assertThat( aggregator.aggregate( Collections.singletonList( call ) ), contains( call ) );
+	}
+
+}
