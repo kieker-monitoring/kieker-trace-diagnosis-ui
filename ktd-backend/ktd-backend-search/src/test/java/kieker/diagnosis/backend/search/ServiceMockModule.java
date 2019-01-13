889c2a57efea5d1a76039956fc76582e007b5b17
Nils Christian Ehmke <nils@rhocas.de>
2019-01-13 08:32:58 +0100

    Refactoring
    Signed-off-by: Nils Christian Ehmke <nils@rhocas.de>

--- ktd-backend/ktd-backend-search/src/test/java/kieker/diagnosis/backend/search/ServiceMockModule.java
+++ ktd-backend/ktd-backend-search/src/test/java/kieker/diagnosis/backend/search/ServiceMockModule.java
@@ -1,3 +1,19 @@
+/***************************************************************************
+ * Copyright 2015-2019 Kieker Project (http://kieker-monitoring.net)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *     http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ ***************************************************************************/
+
 package kieker.diagnosis.backend.search;
 
 import java.util.HashMap;
@@ -9,13 +25,13 @@ import com.google.inject.AbstractModule;
 public final class ServiceMockModule extends AbstractModule {
 
 	private final Map<Class<?>, Object> mockMap;
-	
+
 	public <T> ServiceMockModule( final Class<T> mockClass, final T mockObject ) {
 		this( createMockMap( mockClass, mockObject ) );
 	}
-	
-	private static <T> Map<Class<?>, Object> createMockMap(Class<T> mockClass, T mockObject) {
-		final Map<Class<?>, Object> mockMap = new HashMap<>();
+
+	private static <T> Map<Class<?>, Object> createMockMap( final Class<T> mockClass, final T mockObject ) {
+		final Map<Class<?>, Object> mockMap = new HashMap<>( );
 		mockMap.put( mockClass, mockObject );
 		return mockMap;
 	}
@@ -25,15 +41,15 @@ public final class ServiceMockModule extends AbstractModule {
 	}
 
 	@Override
-	protected void configure() {
-		mockMap.entrySet()
-		       .stream()
-		       .forEach( this::bind );
+	protected void configure( ) {
+		mockMap.entrySet( )
+				.stream( )
+				.forEach( this::bind );
 	}
 
-	@SuppressWarnings("unchecked")
+	@SuppressWarnings ( "unchecked" )
 	private <T> void bind( final Entry<Class<?>, Object> entry ) {
-		bind( ( Class<T> ) entry.getKey( ) ).toInstance( (T) entry.getValue( ) );
+		bind( (Class<T>) entry.getKey( ) ).toInstance( (T) entry.getValue( ) );
 	}
-	
+
 }
